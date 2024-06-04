68e0a52cffacce004a9e9d33be394368636a36d2
Wenjun <2211246@mail.nankai.edu.cn>
2024-06-04 14:40:23 +0800

    startpage v2.0

---
+++ components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="banner-one" id="banner">
+    <span class="banner-one__shape-1"></span>
+    <span class="banner-one__shape-2"></span>
+    <span class="banner-one__shape-3"></span>
+    <span class="banner-one__shape-4"></span>
+    <div class="container">
+      <div class="banner-one__moc">
+        <img src="/assets/images/mocs/banner-moc-1-1.png" alt="Awesome Image" />
+      </div><!-- /.banner-one__moc -->
+      <div class="row">
+        <div class="col-xl-6 col-lg-8">
+          <div class="banner-one__content">
+
+            <h3 class="banner-one__title">童年的<br><span class="game-title">保卫萝卜</span></h3><p class="banner-one__text">We are committed to providing our customers with <br> exceptional service while offering our employees the <br> best training.</p>
+            <!-- /.banner-one__text -->
+            <a href="#" class="banner-one__btn thm-btn "><span>Get Started</span></a><!-- /.thm-btn -->
+          </div><!-- /.banner-one__content -->
+        </div><!-- /.col-lg-6 -->
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+  </section>
+</template>
+
+<script>
+    export default {
+      name: "Banner"
+    }
+</script>
+
+<style scoped>
+
+</style>
