b1f5d797918d96ffc679c1d27e2c418e3ae7fa81
ZhongWishing <3379888411@qq.com>
2024-06-08 18:33:01 +0800

    README.md文件更新

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,99 @@
+<template>
+  <section class="cta-one" id="cta">
+    <img src="/assets/images/background/cta-one-bg.png" class="cta-one__bg" alt="Awesome Image" />
+    <div class="container">
+      <img src="/assets/images/mocs/cta-moc-1-1.png" class="cta-one__moc" alt="Awesome Image" />
+      <div class="row justify-content-lg-end">
+        <div class="col-lg-6">
+          <div class="cta-one__content">
+            <i class="cta-one__icon dimon-icon-data1"></i>
+            <div class="block-title text-left">
+              <h2 class="block-title__title">The Development History of<br><span> Defending Radish</span></h2><!-- /.block-title__title -->
+            </div><!-- /.block-title -->
+            <div class="cta-one__text">
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div><!-- /.cta-one__text -->
+            <ul class="list-unstyled">
+              <li><i class="fa fa-check"></i>Refresing to get such a personal touch.</li>
+              <li><i class="fa fa-check"></i>Duis aute irure dolor in reprehenderit in voluptate.</li>
+              <li><i class="fa fa-check"></i>Velit esse cillum dolore eu fugiat nulla pariatur.</li>
+            </ul><!-- /.list-unstyled -->
+            <a href="/home" class="banner-one__btn thm-btn">
+              <span>Get Started</span>
+            </a>
+
+          </div><!-- /.cta-one__content -->
+        </div><!-- /.col-lg-6 -->
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+  </section>
+</template>
+
+<script>
+    export default {
+      name: "CallToAction"
+    }
+</script>
+
+<style scoped>
+.cta-one {
+  position: relative;
+  overflow: hidden;
+  padding: 100px 0;
+}
+
+.cta-one__moc {
+  transform: translate(115px,100px) !important;
+}
+
+
+.cta-one__content {
+  padding: 30px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+}
+
+.block-title__title {
+  font-size: 28px;
+  font-weight: 700;
+  margin-bottom: 20px;
+}
+
+.cta-one__text p {
+  margin-bottom: 20px;
+}
+
+.list-unstyled {
+  padding-left: 0;
+}
+
+.list-unstyled li {
+  list-style: none;
+  position: relative;
+  padding-left: 20px;
+  margin-bottom: 10px;
+}
+
+.list-unstyled li i {
+  position: absolute;
+  left: 0;
+}
+
+.thm-btn {
+  display: inline-block;
+  padding: 10px 30px;
+  background-color: #007bff;
+  color:black;
+  font-size: 18px;
+  font-weight: 700;
+  text-transform: uppercase;
+  border-radius: 5px;
+  transition: all 0.3s ease;
+}
+
+.thm-btn:hover {
+  background-color: #0056b3;
+}
+
+</style>
---
+++ src/components/Features.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="service-one" id="features">
+    <div class="container">
+      <div class="block-title text-center">
+        <h2 class="block-title__title"><span>Game features</span><br>Enjoy high-quality web games</h2><!-- /.block-title__title -->
+      </div><!-- /.block-title -->
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-duration="1500ms">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner">
+              <i class="service-one__icon dimon-icon-laptop"></i>
+              <h3><a href="#">无需下载，在线畅玩 </a></h3>
+              <p>No downloads required.<br>Play online instantly.<br>Cross-platform.<br> Convenient access.</p>
+              <a href="#" class="service-one__link"><i class="dimon-icon-right-arrow"></i></a>
+            </div><!-- /.service-one__inner -->
+          </div><!-- /.service-one__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12 wow fadeInDown" data-wow-duration="1500ms">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner">
+              <i class="service-one__icon dimon-icon-presentation"></i>
+              <h3><a href="#">高度还原，原汁原味</a></h3>
+              <p>Highly restored.<br>Original taste.<br>Authentic reproduction.<br>Genuine experience.</p>
+              <a href="#" class="service-one__link"><i class="dimon-icon-right-arrow"></i></a>
+            </div><!-- /.service-one__inner -->
+          </div><!-- /.service-one__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12 wow fadeInUp" data-wow-duration="1500ms">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner">
+              <i class="service-one__icon dimon-icon-target"></i>
+              <h3><a href="#">多种模式，拒绝单调</a></h3>
+              <p>Diverse gameplay modes.<br>Rejects monotony.<br>Varied play options.<br>Dynamic gaming.</p>
+              <a href="#" class="service-one__link"><i class="dimon-icon-right-arrow"></i></a>
+            </div><!-- /.service-one__inner -->
+          </div><!-- /.service-one__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12 wow fadeInDown" data-wow-duration="1500ms">
+          <div class="service-one__single text-center">
+            <div class="service-one__inner">
+              <i class="service-one__icon dimon-icon-visualization"></i>
+              <h3><a href="#"> 持续更新，精彩不断</a></h3>
+              <p>Continuous updates.<br>Constant excitement.<br>Endless journey.<br>Continual improvement.</p>
+              <a href="#" class="service-one__link"><i class="dimon-icon-right-arrow"></i></a>
+            </div><!-- /.service-one__inner -->
+          </div><!-- /.service-one__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+  </section>
+</template>
+
+<script>
+    export default {
+      name: "Featires"
+    }
+</script>
+
+<style scoped>
+
+</style>
